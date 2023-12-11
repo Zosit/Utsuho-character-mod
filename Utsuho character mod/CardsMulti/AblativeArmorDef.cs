c82c3ab8f9d10c8c2249608c699a77564d20097f
Zosit <alex.edbrooke.davis@gmail.com>
2023-12-10 19:25:16 -0800

    2 new cards, fixed  Beta Protocol wording

---
+++ Utsuho character mod/CardsMulti/AblativeArmorDef.cs
@@ -0,0 +1,138 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLEntitySideloader.Entities;
+using LBoLEntitySideloader.Resource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Utsuho_character_mod.BepinexPlugin;
+using Utsuho_character_mod.Status;
+using Utsuho_character_mod.Util;
+using HarmonyLib;
+
+namespace Utsuho_character_mod.CardsMulti
+{
+    public sealed class AblativeArmorDef : CardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AblativeArmor);
+        }
+
+        public override CardImages LoadCardImages()
+        {
+            var imgs = new CardImages(BepinexPlugin.embeddedSource);
+            imgs.AutoLoad(this, extension: ".png");
+            return imgs;
+        }
+
+        public override LocalizationOption LoadLocalization()
+        {
+            return UsefulFunctions.LocalizationCard(directorySource);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = new CardConfig(
+                Index: 13527,
+                Id: "",
+                ImageId: "",
+                UpgradeImageId: "",
+                Order: 10,
+                AutoPerform: true,
+                Perform: new string[0][],
+                GunName: "Simple1",
+                GunNameBurst: "Simple1",
+                DebugLevel: 0,
+                Revealable: false,
+                IsPooled: true,
+                HideMesuem: false,
+                IsUpgradable: true,
+                Rarity: Rarity.Rare,
+                Type: CardType.Defense,
+                TargetType: TargetType.Nobody,
+                Colors: new List<ManaColor>() { ManaColor.Black, ManaColor.Red },
+                IsXCost: false,
+                Cost: new ManaGroup() { Black = 1, Red = 1, Any = 1 },
+                UpgradedCost: new ManaGroup() { Any = 3 },
+                MoneyCost: null,
+                Damage: null,
+                UpgradedDamage: null,
+                Block: 15,
+                UpgradedBlock: 20,
+                Shield: 0,
+                UpgradedShield: 0,
+                Value1: 10,
+                UpgradedValue1: 14,
+                Value2: null,
+                UpgradedValue2: null,
+                Mana: null,
+                UpgradedMana: null,
+                Scry: null,
+                UpgradedScry: null,
+                ToolPlayableTimes: null,
+
+                Loyalty: null,
+                UpgradedLoyalty: null,
+                PassiveCost: null,
+                UpgradedPassiveCost: null,
+                ActiveCost: null,
+                UpgradedActiveCost: null,
+                UltimateCost: null,
+                UpgradedUltimateCost: null,
+
+                Keywords: Keyword.Retain,
+                UpgradedKeywords: Keyword.Retain,
+                EmptyDescription: false,
+                RelativeKeyword: Keyword.None,
+                UpgradedRelativeKeyword: Keyword.None,
+
+                RelativeEffects: new List<string>() { },
+                UpgradedRelativeEffects: new List<string>() { },
+                RelativeCards: new List<string>() { "DarkMatter" },
+                UpgradedRelativeCards: new List<string>() { "DarkMatter" },
+                Owner: "Utsuho",
+                Unfinished: false,
+                Illustrator: "",
+                SubIllustrator: new List<string>() { }
+             );
+
+            return cardConfig;            
+        }
+
+        [EntityLogic(typeof(AblativeArmorDef))]
+        public sealed class AblativeArmor : Card
+        {
+            protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+            {
+                if (!base.Battle.BattleShouldEnd)
+                {
+                    this.DeltaShield = 0;
+                    Card card = UsefulFunctions.RandomUtsuho(Battle.HandZone);
+                    while(card.Id == "DarkMatter")
+                    {
+                        yield return new ExileCardAction(card);
+                        this.DeltaShield += Value1;
+                        if (Battle.HandZone.Count != 0)
+                            card = UsefulFunctions.RandomUtsuho(Battle.HandZone);
+                        else
+                            break;
+                    }
+                    yield return base.DefenseAction();
+                    this.DeltaShield = 0;
+
+                    yield break;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Utsuho character mod/CardsMulti/SpacialRendDef.cs
@@ -0,0 +1,139 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLEntitySideloader.Entities;
+using LBoLEntitySideloader.Resource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Utsuho_character_mod.BepinexPlugin;
+using Utsuho_character_mod.Status;
+using Utsuho_character_mod.Util;
+
+namespace Utsuho_character_mod.CardsMulti
+{
+    public sealed class SpacialRendDef : CardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(SpacialRend);
+        }
+
+        public override CardImages LoadCardImages()
+        {
+            var imgs = new CardImages(BepinexPlugin.embeddedSource);
+            imgs.AutoLoad(this, extension: ".png");
+            return imgs;
+        }
+
+        public override LocalizationOption LoadLocalization()
+        {
+            return UsefulFunctions.LocalizationCard(directorySource);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = new CardConfig(
+                Index: 13524,
+                Id: "",
+                ImageId: "",
+                UpgradeImageId: "",
+                Order: 10,
+                AutoPerform: true,
+                Perform: new string[0][],
+                GunName: "Simple1",
+                GunNameBurst: "Simple1",
+                DebugLevel: 0,
+                Revealable: false,
+                IsPooled: true,
+                HideMesuem: false,
+                IsUpgradable: true,
+                Rarity: Rarity.Rare,
+                Type: CardType.Attack,
+                TargetType: TargetType.SingleEnemy,
+                Colors: new List<ManaColor>() { ManaColor.Black, ManaColor.Red },
+                IsXCost: false,
+                Cost: new ManaGroup() { Black = 1, Red = 1, Any = 1 },
+                UpgradedCost: new ManaGroup() { Black = 1, Red = 1, Any = 1 },
+                MoneyCost: null,
+                Damage: 15,
+                UpgradedDamage: 20,
+                Block: null,
+                UpgradedBlock: null,
+                Shield: null,
+                UpgradedShield: null,
+                Value1: 2,
+                UpgradedValue1: 3,
+                Value2: 10,
+                UpgradedValue2: 14,
+                Mana: null,
+                UpgradedMana: null,
+                Scry: null,
+                UpgradedScry: null,
+                ToolPlayableTimes: null,
+
+                Loyalty: null,
+                UpgradedLoyalty: null,
+                PassiveCost: null,
+                UpgradedPassiveCost: null,
+                ActiveCost: null,
+                UpgradedActiveCost: null,
+                UltimateCost: null,
+                UpgradedUltimateCost: null,
+
+                Keywords: Keyword.Retain | Keyword.Exile,
+                UpgradedKeywords: Keyword.Retain | Keyword.Exile,
+                EmptyDescription: false,
+                RelativeKeyword: Keyword.None,
+                UpgradedRelativeKeyword: Keyword.None,
+
+                RelativeEffects: new List<string>() { },
+                UpgradedRelativeEffects: new List<string>() { },
+                RelativeCards: new List<string>() { "DarkMatter" },
+                UpgradedRelativeCards: new List<string>() { "DarkMatter" },
+                Owner: "Utsuho",
+                Unfinished: false,
+                Illustrator: "",
+                SubIllustrator: new List<string>() { }
+             );
+
+            return cardConfig;            
+        }
+
+        [EntityLogic(typeof(SpacialRendDef))]
+        public sealed class SpacialRend : Card
+        {
+            protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+            {
+                if (!base.Battle.BattleShouldEnd)
+                {
+                    this.DeltaDamage = 0;
+                    for (int i = 0; i < Value1; i++)
+                    {
+                        IReadOnlyList<Card> drawZoneIndexOrder = base.Battle.DrawZoneIndexOrder;
+                        Card card = Util.UsefulFunctions.RandomUtsuho(drawZoneIndexOrder);
+                        foreach (BattleAction action in UsefulFunctions.RandomCheck(card, base.Battle)) { yield return action; }
+                        if (card.Id == "DarkMatter")
+                        {
+                            this.DeltaDamage += Value2;
+                        }
+                        yield return new ExileCardAction(card);
+                    }
+
+                    yield return base.AttackAction(selector);
+                    this.DeltaDamage = 0;
+
+                    yield break;
+                }
+            }
+
+        }
+
+    }
+}
